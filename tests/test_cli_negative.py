15d9233657485487337359098f6c93235c079404
Siak Hooi <siakhooi@gmail.com>
2025-04-26 11:31:40 +0000

    fix unit test

--- tests/test_cli_negative.py
+++ tests/test_cli_negative.py
@@ -5,11 +5,9 @@ import pytest
 def test_cli_file_not_found(monkeypatch):
     monkeypatch.setattr(
         "sys.argv",
-        ["download_files_by_map.py", "i-am-not-existing-blahblah.json"],
+        ["download_files_by_map.py", "i-am-blahblah.json"],
     )
+    error = "[Errno 2] No such file or directory: 'i-am-blahblah.json'"  # noqa: E501
     with pytest.raises(FileNotFoundError) as e:
         run()
-    assert (
-        str(e.value)
-        == "[Errno 2] No such file or directory: 'i-am-not-existing-blahblah.json'"
-    )
+    assert str(e.value) == error
